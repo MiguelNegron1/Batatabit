d4dd5e92b6e1b3155a27886d6afa6d6173d3791d
Miguel Negron Garcia <josuefrontend@gmail.com>
2023-09-21 21:34:05 -0500

    Updating the project

---
+++ index.js
@@ -0,0 +1,16 @@
+// const right = document.getElementsByClassName('cripto-data--right-arrow');
+// const left = document.getElementsByClassName('comision-data--left-arrow');
+// const criptoData = document.getElementById('cripto-data-id');
+// const comisionData = document.getElementById('comision-data-id');
+
+// right.addEventListener('click', function() {
+//     console.log('Queee')
+//     criptoData.display = none;
+//     comisionData.display = block;
+// })
+
+// left.addEventListener('click', function () {
+//     console.log('Hola') 
+//     criptoData.display = block;
+//     comisionData.display = none;
+// })
